a5620f697c985494863f416ca23bc37bd364f45e
daawar <daaawar@gmail.com>
2017-08-09 05:09:04 -0700

    Modified this file for a minor bug fix

--- Week2/problem2_3.py
+++ Week2/problem2_3.py
@@ -28,7 +28,7 @@ newEngland = ["Maine","New Hampshire","Vermont", "Rhode Island",
 "Massachusetts","Connecticut"]
 
 def problem2_3(ne):
-    for i in range(0,len(newEngland)):
+    for i in range(0,len(ne)):
         print(ne[i],"has",len(ne[i]),"letters.")
         
 
